a4b0f67f363a71100e0b03a61eab104ee6efbecf
Byeongryul <mapia56@naver.com>
2021-03-11 13:43:36 +0900

    책

---
+++ test.py
@@ -0,0 +1,6 @@
+import os
+os.getcwd()
+os.path.join(os.getcwd(), 'BookChatbot')
+
+import sys
+print(sys.path)
---
+++ train_tools/qna/create_train_data_table.py
@@ -0,0 +1,37 @@
+# 챗봇 데이터 학습용 테이블 생성
+import pymysql
+from config.DatabaseConfig import *
+
+db = None
+
+try:
+    db = pymysql.connect(
+        host = DB_HOST,
+        user = DB_USER,
+        passwd = DB_PASSWARD,
+        db = DB_NAME,
+        charset = 'utf8'
+    )
+
+    # 테이블 생성 sql 정의
+    
+    sql = '''
+    CREATE TABLE IF NOT EXIST 'chatbot_train_data'(
+        'id' INT UNSIGNED NOT NULL AUTO_INCREMENT,
+        'intent' VARCHAR(45) NULL,
+        'ner' VARCHAR(1024) NULL,
+        'query' TEXT NULL,
+        'answer' TEXT NOT NULL,
+        'answer_image' VARCHAR(2048) NULL,
+        PRIMARY KEY ('id'))
+    ENGINE = InnoDB DEFAULT CHARSET-utf8
+    '''
+
+    # 테이블 생성
+    with db.cursor() as cursor:
+        cursor.execute(sql)
+except Exception as e:
+    print(e)
+finally:
+    if db is not None:
+        db.close()
